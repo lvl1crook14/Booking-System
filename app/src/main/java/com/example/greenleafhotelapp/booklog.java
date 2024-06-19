bbcbcd7ade3f763eaa7adc783e8568bb5677e76c
A D M I N <the4thshinobijdgt@gmail.com>
2024-06-19 23:38:41 +0800

    Commit

--- app/src/main/java/com/example/greenleafhotelapp/booklog.java
+++ app/src/main/java/com/example/greenleafhotelapp/booklog.java
@@ -1,6 +1,10 @@
 package com.example.greenleafhotelapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class booklog extends AppCompatActivity {
 
+    private DatePickerDialog datePickerDialog;
+    private Button datebutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_booklog);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initDatePicker();
+        datebutton = findViewById(R.id.date);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+
+                month = month + 1;
+                String date = makeDateString(month, day, year);
+            }
+        };
+    }
+
+    private String makeDateString(int month, int day, int year)
+    {
+        return month + "" + day + "" + year;
+    }
+
+    public void openDatePicker(View view){
+
+
     }
 }
