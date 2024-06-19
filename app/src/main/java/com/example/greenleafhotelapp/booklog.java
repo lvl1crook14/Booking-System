8186c92907ff2e24b3cd494f91a73a6cc30cbc4c
A D M I N <the4thshinobijdgt@gmail.com>
2024-06-19 23:58:41 +0800

    Commit

--- app/src/main/java/com/example/greenleafhotelapp/booklog.java
+++ app/src/main/java/com/example/greenleafhotelapp/booklog.java
@@ -7,10 +7,13 @@ import android.widget.Button;
 import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class booklog extends AppCompatActivity {
 
@@ -24,6 +27,17 @@ public class booklog extends AppCompatActivity {
         setContentView(R.layout.activity_booklog);
         initDatePicker();
         datebutton = findViewById(R.id.date);
+        datebutton.setText(getTodaysDate());
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year  = cal.get(Calendar.YEAR);
+        int month  = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day  = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day,month, year);
     }
 
     private void initDatePicker()
@@ -34,17 +48,56 @@ public class booklog extends AppCompatActivity {
 
                 month = month + 1;
                 String date = makeDateString(month, day, year);
+                datebutton.setText(date);
             }
         };
+
+        Calendar cal = Calendar.getInstance();
+        int year  = cal.get(Calendar.YEAR);
+        int month  = cal.get(Calendar.MONTH);
+        int day  = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
     }
 
     private String makeDateString(int month, int day, int year)
     {
-        return month + "" + day + "" + year;
+        return getMonthFormat(month) + "" + day + "" + year;
     }
 
-    public void openDatePicker(View view){
+    private String getMonthFormat(int month)
+    {
+        if (month == 1)
+            return "JAN";
+        if (month == 2)
+            return "FEB";
+        if (month == 3)
+            return "MAR";
+        if (month == 4)
+            return "APR";
+        if (month == 5)
+            return "MAY";
+        if (month == 6)
+            return "JUN";
+        if (month == 7)
+            return "JUL";
+        if (month == 8)
+            return "AUG";
+        if (month == 9)
+            return "SEP";
+        if (month == 10)
+            return "COT";
+        if (month == 11)
+            return "NOV";
+        if (month == 12)
+            return "DEC";
+
+        return "JAN";
+    }
 
 
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
     }
 }
